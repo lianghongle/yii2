20553874b2e9ecc27056d7dcdde088c698308380
神圣小树 <lianghogle@163.com>
2018-10-23 11:09:38 +0800

    update 优化 YiiHelper

--- strong/helpers/YiiHelper.php
+++ strong/helpers/YiiHelper.php
@@ -13,15 +13,15 @@ class YiiHelper
     /**
      * 对 Yii::$app->params['key'] 的封装
      *
-     * @param $key
-     * @param bool $default
+     * @param $key                      key
+     * @param string $default           默认值
      *
-     * @return bool
+     * @return string
      */
     public static function getAppParams($key, $default = '')
     {
         if (isset(Yii::$app->params[$key])){
-            return Yii::$app->params[$key];
+            return Yii::$app->params[$key] ? Yii::$app->params[$key] : $default;
         }else{
             return $default;
         }
